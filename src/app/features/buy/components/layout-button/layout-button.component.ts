e91c67bc05e2dd565e8603080d63bc34a21a1714
Ana katarines <anakatarines@gmail.com>
2024-06-23 19:21:21 -0300

    refinamento home

--- src/app/features/buy/components/layout-button/layout-button.component.ts
+++ src/app/features/buy/components/layout-button/layout-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-layout-button',
@@ -8,4 +9,10 @@ import { Component, Input } from '@angular/core';
 export class LayoutButtonComponent {
 
 @Input() layout: any;
+
+constructor(private localStorageService: LocalStorageService) {}
+
+setLayout(type: string){
+  this.localStorageService.setItem('layoutList', type);
+}
 }
--- src/app/features/buy/pages/buy-page/buy-page.component.ts
+++ src/app/features/buy/pages/buy-page/buy-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 
 @Component({
@@ -8,14 +10,44 @@ import { ProductService } from '../../services/product.service';
   templateUrl: './buy-page.component.html',
   styleUrls: ['./buy-page.component.scss'],
 })
-export class BuyPageComponent {
+export class BuyPageComponent implements OnInit {
   public list: Product[] = [];
+  layoutList: string | null = null;
+  private storageSubscription: Subscription | undefined;
+  private storageEventListener = (event: StorageEvent) => {
+    if (event.key === 'layoutList') {
+      this.layoutList = event.newValue;
+    }
+  };
+
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private localStorageService: LocalStorageService
   ) {
     this.list = this.productService.allProducts;
   }
+
+  ngOnInit(): void {
+    this.layoutList = this.localStorageService.getItem('layoutList');
+
+    this.storageSubscription = this.localStorageService.watchStorage().subscribe(key => {
+      if (key === 'layoutList') {
+        this.layoutList = this.localStorageService.getItem('layoutList');
+      }
+    });
+
+    window.addEventListener('storage', this.storageEventListener);
+  }
+
+
+  ngOnDestroy(): void {
+    if (this.storageSubscription) {
+      this.storageSubscription.unsubscribe();
+    }
+    window.removeEventListener('storage', this.storageEventListener);
+  }
+
 
 
 }
